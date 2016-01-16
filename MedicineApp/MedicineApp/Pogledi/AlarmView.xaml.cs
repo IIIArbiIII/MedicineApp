f788cc744c7be44aad5fdb85de2cf0932de6b323
Denis Arbeiter <denis.arbeiter@student.um.si>
2016-01-16 18:12:53 +0100

    popravek AlarmView (shitload of errors)

--- MedicineApp/MedicineApp/Pogledi/AlarmView.xaml.cs
+++ MedicineApp/MedicineApp/Pogledi/AlarmView.xaml.cs
@@ -15,14 +15,14 @@ using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
-namespace MedicineApp
+namespace MedicineApp.Pogledi
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class Alarm : Page
+    public sealed partial class AlarmView : Page
     {
-        public Alarm()
+        public AlarmView()
         {
             this.InitializeComponent();
         }
