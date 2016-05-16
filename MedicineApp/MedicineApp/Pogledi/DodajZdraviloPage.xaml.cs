ad7e8d57a55307f57f243a6e727a4f860dab943f
Arbi <Arbi@DESKTOP-DQIPSTU>
2016-05-16 12:44:21 +0200

    Back

--- MedicineApp/MedicineApp/Pogledi/DodajZdraviloPage.xaml.cs
+++ MedicineApp/MedicineApp/Pogledi/DodajZdraviloPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -22,10 +23,14 @@ namespace MedicineApp.Pogledi
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
+    /// 
+
+
     public sealed partial class ZdraviloDodaj : Page
     {
         public ZdraviloDodaj()
         {
+            SystemNavigationManager.GetForCurrentView().AppViewBackButtonVisibility = AppViewBackButtonVisibility.Visible;
             this.InitializeComponent();
         }
 
